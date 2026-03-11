bfcbbfec6f143c0f49fd3d30e6c622ac6a90b603
Alpha <mathematic026@gmail.com>
2026-03-11 16:34:59 +0900

    추가: BaseEntity

---
+++ src/main/java/com/team1/hrbank/entity/base/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.team1.hrbank.entity.base;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  protected Long id;
+
+  @JsonIgnore
+  @CreatedDate
+  @Column(updatable = false)
+  protected Instant createdAt;
+}
