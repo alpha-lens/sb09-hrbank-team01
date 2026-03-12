765a12702031e4b7dcfa494ae967dd19451216c9
이용일 <lyi980403>
2026-03-12 09:23:52 +0900

    backup entity

--- src/main/java/com/team1/hrbank/entity/Backup.java
+++ src/main/java/com/team1/hrbank/entity/Backup.java
@@ -1,34 +1,55 @@
 package com.team1.hrbank.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "backup")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Backup {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(nullable = false)
   private String worker;
 
+  @Column(nullable = false)
   private LocalDateTime createdAt;
 
-  private LocalDateTime endedAt;
+  @Column(nullable = false)
+  private LocalDateTime updatedAt;
 
   @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
   private BackupStatus status;
 
-  @ManyToOne
-  @JoinColumn(name = "file_id")
-  private BinaryContent file;
+  public static Backup startNew(String worker) {
+    Backup backup = new Backup();
+    backup.worker = worker;
+    backup.createdAt = LocalDateTime.now();
+    backup.status = BackupStatus.IN_PROGRESS;
+    return backup;
+  }
+
+  public static Backup skipped(String worker) {
+    Backup backup = new Backup();
+    backup.worker = worker;
+    backup.createdAt = LocalDateTime.now();
+    backup.updatedAt = LocalDateTime.now();
+    backup.status = BackupStatus.SKIPPED;
+    return backup;
+  }
 }
