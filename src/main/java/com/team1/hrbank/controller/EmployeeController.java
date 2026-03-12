efd95f59c308c0be2b390ab105c949e40b9ac848
Alpha <mathematic026@gmail.com>
2026-03-12 18:19:33 +0900

    수정 : EmployeeController EmployeeController 명세에 맞게 생성. 테스트 필요.

---
+++ src/main/java/com/team1/hrbank/controller/EmployeeController.java
@@ -0,0 +1,67 @@
+package com.team1.hrbank.controller;
+
+import com.team1.hrbank.dto.EmployeeDto;
+import com.team1.hrbank.dto.dashboard.EmployeeDistributionDto;
+import com.team1.hrbank.dto.dashboard.EmployeeTrendDto;
+import com.team1.hrbank.dto.request.EmployeeCreateRequest;
+import com.team1.hrbank.dto.request.EmployeeUpdateRequest;
+import com.team1.hrbank.service.EmployeeService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+  private final EmployeeService employeeService;
+
+  @GetMapping
+  public List<EmployeeDto> getEmployees() {
+    return employeeService.findAllEmployees();
+  }
+
+  @GetMapping("/{id}")
+  public EmployeeDto getEmployee(@PathVariable long id) {
+    return employeeService.findEmployee(id);
+  }
+
+  @PostMapping
+  public EmployeeDto createEmployee(@RequestBody EmployeeCreateRequest employeeCreateRequest) {
+    return employeeService.createEmployee(employeeCreateRequest);
+  }
+
+  @PatchMapping("/{id}")
+  public EmployeeDto updateEmployee(@PathVariable long id,
+      @RequestBody EmployeeUpdateRequest employeeUpdateRequest) {
+    return employeeService.updateEmployee(id, employeeUpdateRequest);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteEmployee(@PathVariable long id) {
+    employeeService.deleteEmployee(id);
+  }
+
+  @GetMapping("/stats/trend")
+  public EmployeeTrendDto getEmployeeTrend() {
+    return null;
+  }
+
+  @GetMapping("/stats/disctribution")
+  public EmployeeDistributionDto getEmployeeDistribution() {
+    return null;
+  }
+
+  @GetMapping("/count")
+  public Long countEmployees() {
+    return null;
+  }
+}
