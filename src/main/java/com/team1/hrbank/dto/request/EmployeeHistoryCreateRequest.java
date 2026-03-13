b82e2d610c265c165c9f4af98d22a6dd91a06b63
apple <kjw010900@gmail.com>
2026-03-13 15:54:56 +0900

    수정: - EmployeeHistoryCreate시, ipAddress는 주입받기 위해 제거 - diffJson을 List로 받기위해 타입 변환

--- src/main/java/com/team1/hrbank/dto/request/EmployeeHistoryCreateRequest.java
+++ src/main/java/com/team1/hrbank/dto/request/EmployeeHistoryCreateRequest.java
@@ -1,13 +1,12 @@
 package com.team1.hrbank.dto.request;
 
+import com.team1.hrbank.dto.DiffDto;
 import com.team1.hrbank.entity.HistoryType;
+import java.util.List;
 
 public record EmployeeHistoryCreateRequest(
     HistoryType type,
     String employeeNumber,
-    String diffJson,
-    String memo,
-    String ipAddress
-) {
-
-}
+    List<DiffDto> diffs,
+    String memo
+) {}
