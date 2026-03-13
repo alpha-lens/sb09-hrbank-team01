81139fc81bf294f2806de763e2ed1e91ea842478
ope336 <ope336@gmail.com>
2026-03-13 13:46:08 +0900

    수정: DepartmentRepository 부서 검색 @Query 추가

--- src/main/java/com/team1/hrbank/repository/DepartmentRepository.java
+++ src/main/java/com/team1/hrbank/repository/DepartmentRepository.java
@@ -1,10 +1,15 @@
 package com.team1.hrbank.repository;
 
 import com.team1.hrbank.entity.Department;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
   boolean existsByName(String name);
 
+  @Query("SELECT d FROM Department d WHERE LOWER(d.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR LOWER(d.description) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+  List<Department> searchByKeyword(@Param("keyword") String keyword);
 }
