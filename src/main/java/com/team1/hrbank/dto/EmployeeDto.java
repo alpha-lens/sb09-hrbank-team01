f792d8115d62958e8fb02b2e4ecac5aeb9223c87
Alpha <mathematic026@gmail.com>
2026-03-13 13:42:51 +0900

    수정: EmployeeCreateRequest, EmployeeDto 원시타입 -> Wrapper 타입 반환값을 null로 설정할 수 있도록 하기 위함.

--- src/main/java/com/team1/hrbank/dto/EmployeeDto.java
+++ src/main/java/com/team1/hrbank/dto/EmployeeDto.java
@@ -4,16 +4,16 @@ import com.team1.hrbank.entity.EmployeeStatus;
 import java.time.LocalDate;
 
 public record EmployeeDto(
-    long id,
+    Long id,
     String name,
     String email,
     String employeeNumber,
-    long departmentId,
+    Long departmentId,
     String departmentName,
     String position,
     LocalDate hireDate,
     EmployeeStatus status,
-    long profileImageId
+    Long profileImageId
 ) {
 
 }
