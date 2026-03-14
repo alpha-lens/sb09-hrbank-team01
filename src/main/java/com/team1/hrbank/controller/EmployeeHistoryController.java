fafe12e30954a18f8ea56c4c7613920569b4a845
apple <kjw010900@gmail.com>
2026-03-14 12:27:29 +0900

    수정: PathVariable 어노테이션 추가

--- src/main/java/com/team1/hrbank/controller/EmployeeHistoryController.java
+++ src/main/java/com/team1/hrbank/controller/EmployeeHistoryController.java
@@ -15,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -83,14 +84,11 @@ public class EmployeeHistoryController {
   @GetMapping("/{count}")
   public ResponseEntity<List<EmployeeHistoryDto>> findEmployeeHistoriesByRevisionsBetween(
       @RequestParam(required = false) Long fromRevision,
-      @RequestParam(required = false) Long toRevision
+      @RequestParam(required = false) Long toRevision,
+      @PathVariable Long count
   ) {
     return ResponseEntity.ok(employeeHistoryService.findEmployeeHistoriesByRevisionsBetween(fromRevision, toRevision));
   }
 
-  @GetMapping
-  public ResponseEntity<List<EmployeeHistoryDto>> findAllEmployeeHistories() {
-    return ResponseEntity.ok(employeeHistoryService.findAllEmployeeHistories());
-  }
 
 }
