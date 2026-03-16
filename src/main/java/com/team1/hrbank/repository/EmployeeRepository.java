5e40f83748db99516d34b1d44f0c3082088379e5
Alpha <mathematic026@gmail.com>
2026-03-16 10:49:25 +0900

    수정: EmployeeSpecification Specification을 할 수 있도록 추가함

--- src/main/java/com/team1/hrbank/repository/EmployeeRepository.java
+++ src/main/java/com/team1/hrbank/repository/EmployeeRepository.java
@@ -10,10 +10,12 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long>,
+    JpaSpecificationExecutor<Employee> {
 
   @Query("SELECT MAX(e.employeeNumber) FROM Employee e WHERE e.employeeNumber LIKE :prefix%")
   String findLastEmployeeNameByPrefix(@Param("prefix") String prefix);
