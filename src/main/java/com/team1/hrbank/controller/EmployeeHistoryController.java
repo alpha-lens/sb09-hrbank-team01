9367eb422cb353e6970e0f8301d15a324df8ea74
apple <kjw010900@gmail.com>
2026-03-17 15:51:18 +0900

    수정: @ModelAttrituce 추가(자동 바인딩 대상이지만 임의로 추가)

--- src/main/java/com/team1/hrbank/controller/EmployeeHistoryController.java
+++ src/main/java/com/team1/hrbank/controller/EmployeeHistoryController.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -51,7 +52,7 @@ public class EmployeeHistoryController {
       }
   )
   @GetMapping
-  public ResponseEntity<CursorPageResponseEmployeeHistoryDto> findEmployeeHistories(
+  public ResponseEntity<CursorPageResponseEmployeeHistoryDto> findEmployeeHistories(@ModelAttribute
     EmployeeHistorySearchRequest request) {
     return ResponseEntity.ok(
         employeeHistoryService.findEmployeeHistories(request)
