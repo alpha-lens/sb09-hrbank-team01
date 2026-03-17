c6edf827afef64e4df2202ae112e91c416256a80
ope336 <ope336@gmail.com>
2026-03-17 09:48:22 +0900

    feat: DepartmentService 파라미터 수정

--- src/main/java/com/team1/hrbank/service/DepartmentService.java
+++ src/main/java/com/team1/hrbank/service/DepartmentService.java
@@ -3,6 +3,7 @@ package com.team1.hrbank.service;
 import com.team1.hrbank.dto.DepartmentDto;
 import com.team1.hrbank.dto.cursor.CursorPageResponseDepartmentDto;
 import com.team1.hrbank.dto.request.DepartmentCreateRequest;
+import com.team1.hrbank.dto.request.DepartmentSearchRequest;
 import com.team1.hrbank.dto.request.DepartmentUpdateRequest;
 
 public interface DepartmentService {
@@ -13,9 +14,7 @@ public interface DepartmentService {
 
   DepartmentDto findDepartment(Long id);
 
-  CursorPageResponseDepartmentDto findAllDepartments(
-      String keyword, Long idAfter, String cursor, Integer size, String sortField, String sortDirection
-  );
+  CursorPageResponseDepartmentDto findAllDepartments(DepartmentSearchRequest request);
 
   void deleteDepartment(Long id);
 
