5358550d12b1211004a65ff20ac61f4246129364
Alpha <mathematic026@gmail.com>
2026-03-17 15:08:04 +0900

    수정: Employee 관련된 ENUM Class들 `@JsonCreator`를 이용하여 Uppercase 처리하게 함.

--- src/main/java/com/team1/hrbank/entity/EmployeeStatus.java
+++ src/main/java/com/team1/hrbank/entity/EmployeeStatus.java
@@ -1,5 +1,12 @@
 package com.team1.hrbank.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum EmployeeStatus {
-  ACTIVE, ON_LEAVE, RESIGNED
+  ACTIVE, ON_LEAVE, RESIGNED;
+
+  @JsonCreator
+  public static EmployeeStatus from(String s) {
+    return EmployeeStatus.valueOf(s.toUpperCase());
+  }
 }
