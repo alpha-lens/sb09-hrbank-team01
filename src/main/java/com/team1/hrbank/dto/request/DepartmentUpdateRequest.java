c379ec210a63bfdea65295f4418acf6654aee9fa
ope336 <ope336@gmail.com>
2026-03-17 13:38:17 +0900

    fix: DepartmentUpdateRequest 수정

--- src/main/java/com/team1/hrbank/dto/request/DepartmentUpdateRequest.java
+++ src/main/java/com/team1/hrbank/dto/request/DepartmentUpdateRequest.java
@@ -3,9 +3,9 @@ package com.team1.hrbank.dto.request;
 import java.time.LocalDate;
 
 public record DepartmentUpdateRequest(
-    String newName,
-    String newDescription,
-    LocalDate newEstablishedDate
+    String name,
+    String description,
+    LocalDate establishedDate
 ) {
 
 }
