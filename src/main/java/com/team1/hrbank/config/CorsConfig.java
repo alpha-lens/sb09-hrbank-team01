07f91d71761370ec9b07ac9bca570ba2efbb8f3d
Alpha <mathematic026@gmail.com>
2026-03-19 15:10:03 +0900

    수정: CorsConfig, application-product.yml CORS Error 해결을 위한 수정 테스트 파일

--- src/main/java/com/team1/hrbank/config/CorsConfig.java
+++ src/main/java/com/team1/hrbank/config/CorsConfig.java
@@ -2,23 +2,24 @@ package com.team1.hrbank.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
 
   @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOriginPatterns("*")
-            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true);
-      }
-    };
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+
+    config.setAllowCredentials(true);
+    config.addAllowedOriginPattern("https://sb09-hrbank-team01-production-6d26.up.railway.app"); // 3번 참고: 가급적 실제 URL 명시 권장
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
   }
 }
